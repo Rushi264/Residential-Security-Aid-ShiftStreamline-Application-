6fb6f2a64efd3858556a2c009346eaff9003d619
Aishwarya Nadkarni <aishwaryanadkarni@aishwaryas-air.mynetworksettings.com>
2024-04-15 23:32:31 -0400

    Added code logic view, login controller and authnetication

---
+++ Login Page/Backend/Authentication Provider.java
@@ -0,0 +1,7 @@
+@Override
+protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("admin")
+        .password("{noop}pass") // {noop} for plain text, usually you would encode this
+        .roles("USER");
+}
